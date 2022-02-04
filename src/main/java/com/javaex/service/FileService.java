ba98e649e1a2dbcc99c1bd641ce66933f6376c97
황일영 <codeclz2021@gmail.com>
2022-02-04 15:00:52 +0900

    mysite4

--- src/main/java/com/javaex/service/FileService.java
+++ src/main/java/com/javaex/service/FileService.java
@@ -1,5 +1,9 @@
 package com.javaex.service;
 
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
@@ -8,26 +12,46 @@ import org.springframework.web.multipart.MultipartFile;
 @Service
 public class FileService {
 
-	public void restore(MultipartFile file) {
+	public String restore(MultipartFile file) {
 		System.out.println("FileService.restore()");
-		
-		//파일을 하드디스크에 저장(운영내용)
-		
-		//원본파일이름
+		String saveDir = "C:\\javaStudy\\upload";
+
+		// 파일관련 정보 추출
+
+		// 원본파일이름
 		String orgName = file.getOriginalFilename();
-		
-		//확장자
+
+		// 확장자
 		String exName = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf("."));
-				
-		//저장파일이름		
+
+		// 저장파일이름
 		String saveName = System.currentTimeMillis() + UUID.randomUUID().toString() + exName;
-		System.out.println(saveName);
+
+		// 파일패스 생성
+		String filePath = saveDir + "\\" + saveName;
+
+		// 파일 사이즈
+		long fileSize = file.getSize();
+
+		// ***파일 저장(업로드)
+		try {
+			byte[] fileData = file.getBytes();
+			OutputStream out = new FileOutputStream(filePath);
+			BufferedOutputStream bout = new BufferedOutputStream(out);
+
+			bout.write(fileData);
+			bout.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		// DB에 저장
+		// 과제
 		
 		
-		//DB에 저장
+		return saveName;
 		
 		
 	}
-	
-	
+
 }
